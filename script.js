380a5b42858badac3f8ee19ff2be973bebe7762f
darthommen <io@vibygym.dk>
2022-08-30 21:48:45 +0200

    Start på js

---
+++ script.js
@@ -0,0 +1,9 @@
+const numberButtons = document.querySelectorAll('[data-num')
+const operationButtons = document.querySelectorAll('[data-op')
+const equalsButton = document.querySelector('[data-eq')
+const deleteButton = document.querySelector('[data-del')
+const AllClearButton = document.querySelector('[data-allclear')
+const deleteButton = document.querySelector('[data-del')
+previousOperandTextElement
+
+
