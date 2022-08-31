6d463c1f2363372da13a0d72cbae8719b4dc8d9e
darthommen <io@vibygym.dk>
2022-08-31 15:17:51 +0200

    lidt flere funktioner, stadig ikke færdig

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+}
+
+clear() {
+
+}
+
+delete() {
+
+}
+
+appendNumber(Number) {
+
+}
+
+chooseOperation(operation) {
+
+}
+
+compute() {
+
+}
+
+updateDisplay() {
+    
+}
+
 const numberButtons = document.querySelectorAll('[data-num')
 const operationButtons = document.querySelectorAll('[data-op')
 const equalsButton = document.querySelector('[data-eq')
 const deleteButton = document.querySelector('[data-del')
 const AllClearButton = document.querySelector('[data-allclear')
 const deleteButton = document.querySelector('[data-del')
-previousOperandTextElement
-
+previousOperandTextElement = document.querySelector('[data-pre-op]')
+currentOperandTextElement = document.querySelector('[data-cur-op]')
 
