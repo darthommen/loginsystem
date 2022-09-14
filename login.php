da485d60a9757654459604aa3a7502e27a3a94cc
darthommen <io@vibygym.dk>
2022-09-14 21:24:38 +0200

    jippi

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
      <section class="loginform">
         <div class="wrapper">
           <h2>Log in</h2>
-          <form action="login.inc.php" method="post">
+          <form action="includes/login.inc.php" method="post">
             <input type="text" name="uid" placeholder="Brugernavn..."><br>
             <input type="password" name="pwd" placeholder="Password..."><br>
             <button type="submit" name="submit">Log In</button><br>
--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
      <section class="signupform">
         <div class="wrapper">
           <h2>Sign Up</h2>
-          <form action="signup.inc.php" method="post">
+          <form action="includes/signup.inc.php" method="post">
             <input type="text" name="uid" placeholder="Brugernavn..."><br>
             <input type="password" name="pwd" placeholder="Password..."><br>
             <input type="password" name="pwdrepeat" placeholder="Repeat password..."><br>
