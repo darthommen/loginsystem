9ae7319c34477ba58a9a390dd587595cc85680b3
darthommen <io@vibygym.dk>
2022-09-18 21:03:51 +0200

    der er en fejl i login

---
+++ includes/functions.inc.php
@@ -0,0 +1,109 @@
+<?php
+
+function emptyInputSignup($username,$pwd,$pwdRepeat) {
+    $result;
+    if (empty($username) || empty($pwd) || empty($pwdRepeat)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function invalidUid($username) {
+    $result;
+    if (!preg_match("/^[a-zA-Z0-9]*$/", $username)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function pwdMatch($pwd, $pwdRepeat) {
+    $result;
+    if ($pwd !== $pwdRepeat) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function uidExists($conn, $username) {
+    $sql = "SELECT * FROM login WHERE brugernavn = ?;";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../signup.php?error=stmtfailed");
+        exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "s", $username);
+    mysqli_stmt_execute($stmt);
+
+    $resultData = mysqli_stmt_get_result($stmt);
+
+    if ($row = mysqli_fetch_assoc($resultData)) {
+        return $row;
+    }
+    else {
+        $result = false;
+        return $result;
+    }
+
+    mysqli_stmt_close($stmt);
+}
+
+function createUser($conn, $username, $pwd) {
+    $sql = "INSERT INTO login (brugernavn, password) VALUES (?, ?);";
+    $stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        header("location: ../signup.php?error=stmtfailed");
+        exit();
+    }
+
+    $hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+
+    mysqli_stmt_bind_param($stmt, "ss", $username, $hashedPwd);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_close($stmt);
+    header("location: ../signup.php?error=none");
+    exit();
+}
+
+function emptyInputLogin($username,$pwd) {
+    $result;
+    if (empty($username) || empty($pwd)) {
+        $result = true;
+    }
+    else {
+        $result = false;
+    }
+    return $result;
+}
+
+function  loginUser($conn, $username, $pwd) {
+    $uidExists = uidExists($conn, $username);
+
+    if ($uidExists === false) {
+        header("location:../login.php?error=wronglogin");
+        exit();
+    }
+
+    $pwdHashed = $uidExists["password"];
+    $checkPwd = password_verify($pwd, $pwdHashed);
+
+    if ($checkPwd === false) {
+        header("location:../login.php?error=wronglogin2");
+        exit();
+    }
+    else if ($checkPwd === true) {
+        session_start();
+        $_SESSION["username"] = $uidExists["username"];
+        header("location:../index.php");
+        exit();
+    }
+}
--- login.php
+++ login.php
@@ -11,6 +11,25 @@
             <button type="submit" name="submit">Log In</button><br>
           </form>
         </div>
+        <?php
+          if (isset($_GET["error"])) {
+            if($_GET["error"] == "emptyinput") {
+              echo "<p>Udfyld alle felter!</p>";
+            }
+            
+            else if($_GET["error"] == "wronglogin") {
+              echo "<p>Forkert brugernavn eller password!</p>";
+            }
+            else if($_GET["error"] == "wronglogin2") {
+              echo "<p>Forkert brugernavn eller password!</p>";
+            }
+            else if($_GET["error"] == "none") {
+              echo "<p>Du har signed up!</p>";
+            }
+          
+          }      
+        
+        ?>
      </section>
   
 
